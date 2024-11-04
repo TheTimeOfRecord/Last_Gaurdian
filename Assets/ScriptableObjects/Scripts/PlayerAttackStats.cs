8b5d75c8bd86cbe97fb864eda32a1b0855301b32
TheTimeOfRecord <131391796+TheTimeOfRecord@users.noreply.github.com>
2024-11-04 12:43:07 +0900

    Add PlayerAttackStats.cs

---
+++ Assets/ScriptableObjects/Scripts/PlayerAttackStats.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerAttackStats", menuName = "Player/AttackStats")]
+public class PlayerAttackStats : ScriptableObject
+{
+    [Header("Attack")]
+    public float attackDamage;
+    public float attackDelay;
+}
