70a7001740cfd1b27afa817baf71c537dddecb1b
Mainoo-developer <hyunw0629@naver.com>
2024-11-07 13:34:15 +0900

    fix : Attack

--- Assets/Scripts/Weapons/Weapon.cs
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -1,35 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    // public WeaponData weaponData;
-    public int attackDamage = 10; // -> WeaponData 생기면 없앤다
-    public Collider weaponCollier;
-
-
-    private void Awake()
-    {
-        if (weaponCollier != null)
-        {
-            weaponCollier.enabled = false;
-        }
-    }
-    private void Start()
-    {
-        if (weaponCollier != null)
-        {
-            weaponCollier.enabled = false;
-        }
-    }
+    public EquipmentItem equipmentItem;
     private void OnTriggerEnter(Collider other)
     {
         IDamagable damgable = other.GetComponent<IDamagable>();
         if (damgable != null)
         {
-            damgable.TakeDamage(attackDamage, transform);
-            // WeaponData 생기면 attackDamge ->  weaponData.attackDamge (변경)
+            damgable.TakeDamage((int)equipmentItem.attackDamage, transform);
         }
     }
 }
